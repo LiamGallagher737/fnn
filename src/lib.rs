ab6a5563153b420afa3dcbea003bbcb88c36108a
Liam Gallagher <liam@liamgallagher.dev>
2024-11-15 19:00:45 +1300

    Crate level docs (#11)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
+//! To create a new neural network you can use the following. This creates a network that takes two inputs, has two hidden neurons and gives one output.
+//!
+//! ```rust,no_run
+//! # use fnn::prelude::*;
+//! let mut nn = FeedForward::<Sigmoid, 2, 2, 1>::new();
+//! ```
+//!
+//! Then given some training data like this:
+//!
+//! ```rust,no_run
+//! let training_data = [
+//!     ([0.0, 0.0], [0.0]),
+//!     ([0.0, 1.0], [1.0]),
+//!     ([1.0, 0.0], [1.0]),
+//!     ([1.0, 1.0], [0.0]),
+//! ];
+//! ```
+//!
+//! You can train the network a few times:
+//!
+//! ```rust,no_run
+//! # use fnn::prelude::*;
+//! # let mut nn = FeedForward::<Sigmoid, 2, 2, 1>::new();
+//! # let training_data = [
+//! #     ([0.0, 0.0], [0.0]),
+//! #     ([0.0, 1.0], [1.0]),
+//! #     ([1.0, 0.0], [1.0]),
+//! #     ([1.0, 1.0], [0.0]),
+//! # ];
+//! for _ in 0..50_000 {
+//!     for (input, target) in &training_data {
+//!         let input = SVector::from_column_slice(input);
+//!         let target = SVector::from_column_slice(target);
+//!         nn.train(&input, &target, 0.1);
+//!     }
+//! }
+//! ```
+//!
+//! Then get a prediction:
+//!
+//! ```rust
+//! # use fnn::prelude::*;
+//! # let nn = FeedForward::<Sigmoid, 2, 2, 1>::new();
+//! let output = nn.forward(&SVector::from_column_slice(&[0.0, 1.0]));
+//! ```
+//!
+//! The [full example](examples/predict_xor.rs) can produce decently accurate results with these parameters:
+//!
+//! ```text
+//! Input: [0.0, 0.0], Output: 0.015919467, Expected: 0, Accuracy: 98.40805%
+//! Input: [0.0, 1.0], Output: 0.9832184, Expected: 1, Accuracy: 98.32184%
+//! Input: [1.0, 0.0], Output: 0.98321366, Expected: 1, Accuracy: 98.321365%
+//! Input: [1.0, 1.0], Output: 0.020730482, Expected: 0, Accuracy: 97.92695%
+//! ```
+
 #![no_std]
 
 use core::marker::PhantomData;
